0b9442df3ec99b11b7212c9bd9f935e977787a58
Akhand Pratap Singh <ankitsingha4p7s@gmail.com>
2025-12-03 16:53:40 +0530

    changed intro

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,8 +56,8 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h1>{`Hi! I'm Akhand`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "MERN Developer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Hi, I’m Akhand Pratap Singh a full-stack MERN developer passionate about building clean, responsive, and scalable web applications. I enjoy turning ideas into real products and constantly improving my skills through hands-on projects and problem-solving.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
